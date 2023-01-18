57da42fb24796143e63fa1db0e18a671c50a94dd
lisa <lisa.fe.wga@gmail.com>
2023-01-18 13:08:37 +0200

    finish hw7

--- index.js
+++ index.js
@@ -1,118 +1,81 @@
-1;
+// 1
+function searchCandidatesByPhoneNumber(phone) {
+  const normalizedPhoneNum = (string) => string.replaceAll(/\D/g, '');
+  const incomingPhoneString = normalizedPhoneNum(phone);
 
-for (let i = 1; i <= 10; i++) {
-  if (i % 2 === 0) {
-    console.log('index: ', i, 'Fiz');
-  } else if (i % 3 === 0) {
-    console.log('index: ', i, 'FizBuz');
-  } else {
-    console.log('index: ', i, 'Buz');
+  return candidates.filter((c) => {
+    const candidatePhone = normalizedPhoneNum(c.phone);
+    return candidatePhone.includes(incomingPhoneString) || candidatePhone.startsWith(incomingPhoneString);
+  });
+}
+
+const test1 = searchCandidatesByPhoneNumber('43');
+// console.log(test1);
+const test2 = searchCandidatesByPhoneNumber('+1(869) 40');
+// console.log(test2);
+const test3 = searchCandidatesByPhoneNumber('+1(869)408-39-82');
+// console.log(test3);
+
+// 2
+const padDigits = (digit) => digit.toString().padStart(2, '0');
+
+function getCandidateById(id) {
+  const formatRegisteredDate = (candidate) => {
+    const date = new Date(candidate.registered);
+    const month = padDigits(date.getUTCMonth() + 1);
+    const day = padDigits(date.getUTCDay() + 1);
+    const year = date.getUTCFullYear().toString().slice(-2);
+    const registered = `${day}/${month}/${year}`;
+
+    return { ...candidate, registered };
+  }
+
+  const candidate = candidates.find(c => c._id === id);
+
+  return candidate ? formatRegisteredDate(candidate) : undefined;
+}
+
+const test4 = getCandidateById('5e216bc9a6059760578aefa4');
+// console.log(test4);
+
+// 3
+function sortCandidatesArrByBalance(order = '') {
+  if (!order) {
+    return candidates;
+  }
+
+  const normalizedBalance = (string) => string.replaceAll(/\D/g, '');
+
+  const inAscendingOrder = () => [...candidates].sort((c1, c2) => normalizedBalance(c1.balance) - normalizedBalance(c2.balance));
+  const inDescendingOrder = () => [...candidates].sort((c1, c2) => normalizedBalance(c2.balance) - normalizedBalance(c1.balance));
+
+  switch (order) {
+    case 'asc':
+      return inAscendingOrder();
+    case 'desc':
+      return inDescendingOrder();
+    default:
+      return candidates;
   }
 }
 
-2;
+// console.log(sortCandidatesArrByBalance('asc'));
+// console.log(sortCandidatesArrByBalance('desc'));
+// console.log(sortCandidatesArrByBalance());
 
-const factorial = (num) => {
-  if (num === 0) return 0;
+// 4
 
-  let result = 1;
+function getEyeColorMap() {
+  const map = new Map();
 
-  for (let i = num; i > 0; i--) {
-    result *= i;
+  for (const candidate of candidates) {
+    const candidatesByEyeColor = map.get(candidate.eyeColor) || [];
+
+    map.set(candidate.eyeColor, [ ...candidatesByEyeColor, candidate ]);
   }
 
-  return result;
-};
-
-console.log('factorial: ', factorial(10));
-
-3;
-
-const calculatePaperStackNum = (
-  sheetsInStack,
-  weeklyConsumption,
-  weekCount
-) => {
-  const consumptionPerWeeks = weekCount * weeklyConsumption;
-  let requiredNumOfStacks = consumptionPerWeeks / sheetsInStack;
-
-  if (requiredNumOfStacks % 1 !== 0) {
-    requiredNumOfStacks = (requiredNumOfStacks >> 0) + 1;
-  }
-
-  return requiredNumOfStacks;
-};
-
-const sheetsInReamPaper = 500;
-const consumptionPerWeek = 1200;
-const weeksAmount = 8;
-
-console.log(
-  'required paper stack num: ',
-  calculatePaperStackNum(sheetsInReamPaper, consumptionPerWeek, weeksAmount)
-);
-
-4;
-
-function locateRoom(room, roomsOnFloor, floors) {
-  if (room < 100) {
-    throw new Error('Number must contain three digits.');
-  }
-
-  const roomsPerPorch = floors * roomsOnFloor;
-
-  const start = room - 1;
-  const porch = (start / roomsPerPorch + 1) << 0;
-  const floor = ((start - (porch - 1) * roomsPerPorch) / roomsOnFloor + 1) << 0;
-
-  return { porch, floor };
+  return map;
 }
 
-const roomsOnFloor = 3;
-const floors = 9;
-const roomNumber = 456;
-
-console.log(
-  "room's porch and floor: ",
-  locateRoom(roomNumber, roomsOnFloor, floors)
-);
-
-// 5
-
-const pyramid = (medianNumber) => {
-  let resultingString = '';
-
-  for (let row = 1; row <= medianNumber; row++) {
-    for (let spaceLeft = row; spaceLeft < medianNumber; spaceLeft++) {
-      resultingString += '-';
-    }
-
-    for (
-      let buildingBlockLeft = 1;
-      buildingBlockLeft <= row;
-      buildingBlockLeft++
-    ) {
-      resultingString += '#';
-    }
-
-    for (
-      let buildingBlockRight = 1;
-      buildingBlockRight < row;
-      buildingBlockRight++
-    ) {
-      resultingString += '#';
-    }
-
-    for (let spaceRight = medianNumber; spaceRight > row; spaceRight--) {
-      resultingString += '-';
-    }
-
-    resultingString += '\n';
-  }
-
-  console.log('pyramid from median', medianNumber);
-  return resultingString;
-};
-
-console.log(pyramid(6));
-console.log(pyramid(8));
+const eyeColorMap = getEyeColorMap();
+console.log(eyeColorMap);
